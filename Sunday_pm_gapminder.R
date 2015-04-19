e202a9f3c86946da883c684b4a39f069d546d69e
susanh <susanhammerly@unt.edu>
2015-04-19 14:00:59 -0500

    playing with functions (not so successful)

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -5,3 +5,18 @@
 
 #read in data as tab-deliniated
 gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
+
+for (row.number in 1:10){
+  for (col.number in 1:5){
+    print(gap.in[row.number,col.number])
+  }
+}
+
+
+gap.in[1:10,1:5]
+
+add.me <- function(x, y){
+  (x + y) * 8
+}
+
+add.me(3,4)
